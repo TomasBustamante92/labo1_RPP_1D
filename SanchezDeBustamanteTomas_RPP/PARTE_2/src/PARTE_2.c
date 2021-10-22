4a8d6a375ffea8671d9bc96651337350b7ec682b
TomasBustamante92 <tom.sb92@gmail.com>
2021-10-22 13:04:30 -0500

    Parte 2

---
+++ SanchezDeBustamanteTomas_RPP/PARTE_2/src/PARTE_2.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+
+	Recuperatorio Primer Parcial de Laboratorio I
+
+	Alumno: Tomas Sanchez de Bustamante
+	Division: D
+	OS: Linux
+
+ ============================================================================
+ */
+
+#include <stdio_ext.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout, NULL);
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
